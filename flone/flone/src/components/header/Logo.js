6f0aa3f1a10abf03f0cfaa720a77b80227f08a7d
Poornima Rajendran <Poornima.Rajendran@ocuco.com>
2021-05-20 16:01:21 +0100

    added files

---
+++ flone/flone/src/components/header/Logo.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Link } from "react-router-dom";
+import {getConfig} from "../../helpers/ls"
+
+const Logo = ({ imageUrl, logoClass }) => {
+  const config = getConfig();
+  const styles = {
+    color: config.theme.color
+  }
+  console.log('config',config);
+  return (
+    <div className={`${logoClass ? logoClass : ""}`} >
+      <Link to={process.env.PUBLIC_URL + "/"} style={styles}>
+        {config.name}
+        {/* <img alt="" src={process.env.PUBLIC_URL + imageUrl} /> */}
+      </Link>
+    </div>
+  );
+};
+
+Logo.propTypes = {
+  imageUrl: PropTypes.string,
+  logoClass: PropTypes.string
+};
+
+export default Logo;
